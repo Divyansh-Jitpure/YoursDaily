5b2ca60f40b010e5cf3d146d1a3dbd47b53cae99
Divyansh Jitpure <divyanshjit2022@gmail.com>
2025-01-17 22:01:21 +0530

    update

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      {/* Navbar */}
+      <nav></nav>
+      {/* Hero */}
+      <section></section>
+      {/* Content */}
+      <section></section>
+      {/* Footer */}
+      <footer></footer>
+    </div>
+  );
 };
 
 export default Home;
